fa3b0c49f2e2533ed0fe53e219a0445774be9557
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 23:11:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.count {|str| valid_calls.member? str }
+  planeteer_calls.count { |str| valid_calls.member? str }
   
 end
       
