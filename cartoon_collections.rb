8c546592b2f2480b33e7579561ef8fc560dc8403
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 22:45:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.member? {|str| str & valid_calls}
+  planeteer_calls.count {|str| str & valid_calls}
   
   
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
