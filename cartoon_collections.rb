1f928913ba8a2dc1515477f92a33a590b8064e25
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 23:02:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,6 +30,7 @@ def find_valid_calls(planeteer_calls)
     end
     i += 1 
   end
+  
 
   
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
